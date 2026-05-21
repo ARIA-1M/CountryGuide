2e7fb902c8dc983383cfafec4979bb3e57c701b7
unknown <@ngknn.local>
2026-05-21 12:30:43 +0300

    перенос проекта

---
+++ GC/guide_project/guide_project/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+<<<<<<< HEAD
+    path('', include('guide.urls')),
+=======
+    path('', include('guides.urls')),
+>>>>>>> developP
+]
+
+
---
+++ GC/guide_project/guides/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'guides'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('country_create/', views.country_create, name='country_create'),
+]
